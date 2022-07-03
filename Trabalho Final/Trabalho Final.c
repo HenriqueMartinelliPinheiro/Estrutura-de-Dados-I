b36c928cd54743f4a70fff3865ae5aa79a82f1c5
henriquePinheiro <48884506+HenriqueMartinelliPinheiro@users.noreply.github.com>
2022-07-03 19:25:37 -0300

    Versão Faltando QuickSort
    Versão faltando terminar o QuickSort .

---
+++ Trabalho Final/Trabalho Final.c
@@ -0,0 +1,581 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define CHAVES 53
+
+typedef struct sElemento{
+	struct sElemento *next;
+	struct sElemento *prev;
+	char nome [20];
+} Elemento;
+
+typedef struct sIndice{
+	struct sElemento *head;
+	struct sElemento *tail;
+	struct sIndice *prev;
+	struct sIndice *next;
+	
+	int indice;
+	int size;
+} Indice;
+
+typedef struct sLista{
+    int size;
+    struct sIndice *head;
+    struct sIndice *tail;
+} Lista;
+
+Lista* alocarLista();
+Elemento* alocarElemento();
+Indice* alocarIndice();
+int emptyIndice(Indice*);
+int emptyLista(Lista*);
+void limparIndice(Indice*);
+void limparLista(Lista*);
+void remover(Indice*,Elemento*);
+int inserirDado(char[],Indice*, Elemento*);
+void percorrerIndice(Indice*);
+int inserirIndice(Lista*, Indice*);
+Indice* encontrarIndice(Lista*, int);
+void inserirAllIndices(Lista*);
+int hash(char[]);
+FILE* carregarArquivo();
+void inserirAllNomes(Lista*);
+void imprimirSizes(Lista*);
+void removerIndice(Lista *, Indice *);
+int encontrarPosicaoNome(Indice*, char[]);
+void imprimirPosicaoNome(Lista*, char[]);
+Indice* removerNomeEspecifico(Lista*, char[]);
+Elemento* encontrarMeio(int,Indice*);
+void quickSort(Indice *, int, int);
+void changeNome(Elemento*,Elemento*);
+Indice* chamarInserir(Lista* ,char []);
+char* lerNome();
+void menu(Lista*);
+void ordenarAll(Lista* l);
+
+int main(){
+	
+    Lista* lista  = alocarLista();
+
+	if (lista==NULL){
+		return 0;
+	}
+
+	inserirAllIndices(lista);
+	inserirAllNomes(lista);
+	menu(lista);
+	limparLista(lista);
+	// Indice* i = encontrarIndice(lista,hash(nome1));
+	// inserirDado(nome1,i,i->tail);
+	// inserirDado(nome1,i,i->tail);
+	// removerNomeEspecifico(lista,nome1);
+
+
+	// Indice* i = encontrarIndice(lista,20);
+
+	//percorrerIndice(i);
+	// inserirDado("CARLOS",encontrarIndice(lista,hash("CARLOS")),encontrarIndice(lista,hash("CARLOS"))->tail);
+	// imprimirSizes(lista);
+	// encontrarIndice(lista,hash("CARLOS"));
+	
+	// imprimirPosicaoNome(lista,"RAILOM");
+	// percorrerIndice(encontrarIndice(lista,20));
+
+	// imprimirSizes(lista);
+	// printf("Hash: %i",hash("CARLOS"));
+	// percorrerIndice(encontrarIndice(lista,51));
+
+
+	return 0;
+}
+
+Lista *alocarLista(){
+	Lista *l = (Lista *)malloc(sizeof(Lista));
+	if (l == NULL)
+	{
+		verificarErro(-1);
+	}
+
+	l->head = NULL;
+	l->tail = NULL;
+	l->size = 0;
+	return l;
+}
+
+Elemento *alocarElemento(){
+	Elemento *e = (Elemento *)malloc(sizeof(Elemento));
+	if (e == NULL)
+	{
+		verificarErro(-1);
+	}
+	e->next =NULL;
+	e->prev =NULL;
+	return e;
+}
+
+Indice *alocarIndice(){
+    Indice *i = (Indice*)malloc(sizeof(Indice));
+	if (i == NULL)
+	{
+		verificarErro(-1);
+	}
+
+    i->next = NULL;
+    i->prev = NULL;
+    i->head = NULL;
+    i->tail = NULL;
+    i->size=0;
+	i->indice=0;
+}
+
+int inserirDado(char nome[], Indice *i, Elemento* pivo){
+	Elemento *e = alocarElemento();
+
+	strcpy(e->nome,nome);
+	
+	if (pivo == NULL && emptyIndice(i)== 1)
+	{
+		return -2;
+	}
+
+	if (emptyIndice(i) == 0)
+	{
+		i->head = e;
+		i->tail = e;
+	}
+	else
+	{
+		e->next = pivo->next;
+		e->prev = pivo;
+
+		if (pivo->next == NULL)
+		{
+			i->tail = e;
+		}
+		else
+		{
+			pivo->next->prev = e;
+		}
+		pivo->next = e;
+	}
+	i->size++;
+	return 0;
+}
+
+int inserirIndice(Lista *l, Indice* pivo){
+	Indice *i = alocarIndice();
+  	i->indice = l->size;
+	
+	if (pivo == NULL && emptyLista(l)== 1)
+	{
+		return -2;
+	}
+
+	if (emptyLista(l) == 0)
+	{
+		l->head = i;
+		l->tail = i;
+	}
+	else
+	{
+		i->next = pivo->next;
+		i->prev = pivo;
+
+		if (pivo->next == NULL)
+		{
+			l->tail = i;
+		}
+		else
+		{
+			pivo->next->prev = i;
+		}
+		pivo->next = i;
+	}
+	l->size++;
+	return 0;
+}
+
+
+void remover(Indice *i, Elemento *pivo){
+	if (pivo != NULL && emptyIndice(i) == 1)
+	{
+		if (pivo == i->head)
+		{
+			i->head = pivo->next;
+			if (i->head == NULL)
+			{
+				i->tail = NULL;
+			}
+			else
+			{
+				pivo->next->prev = NULL;
+			}
+		}
+		else
+		{
+			pivo->prev->next = pivo->next;
+			if (pivo->next == NULL)
+			{
+				i->tail = pivo->prev;
+			}
+			else
+			{
+				pivo->next->prev = pivo->prev;
+			}
+		}
+		free(pivo);
+		i->size--;
+	}
+}
+
+void removerIndice(Lista *l, Indice *i){
+	if (i != NULL && emptyIndice(i) == 1)
+	{
+		if (i == l->head)
+		{
+			l->head = i->next;
+			if (l->head == NULL)
+			{
+				l->tail = NULL;
+			}
+			else
+			{
+				i->next->prev = NULL;
+			}
+		}
+		else
+		{
+			i->prev->next = i->next;
+			if (i->next == NULL)
+			{
+				l->tail = i->prev;
+			}
+			else
+			{
+				i->next->prev = i->prev;
+			}
+		}
+		free(i);
+		l->size--;
+	}
+}
+
+int emptyLista(Lista* l){
+    if (l->size==0){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
+int emptyIndice(Indice* i){
+    if (i->size==0){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
+void limparIndice(Indice * i){
+    Elemento *e;
+    Elemento *exc;
+
+    e = i->head;
+
+    while (e != NULL)
+    {
+        exc = e;
+        e = e->next;
+        remover(i,exc);
+    }
+}
+
+void limparLista(Lista * l){
+    Indice *exc;
+    Indice *i;
+
+    i = l->head;
+
+    while (i != NULL)
+    {
+        exc = i;
+        i = i->next;
+        limparIndice(exc);
+		removerIndice(l,exc);
+    }
+    free(l);
+}
+
+void percorrerIndice(Indice *i){
+	Elemento *aux;
+	aux = i->head;
+
+	while (aux != NULL)
+	{
+		printf("%s \n", aux->nome);
+		aux = aux->next;
+	}
+}
+
+Indice* encontrarIndice(Lista* l, int chave){
+	Indice* aux = l->head;
+
+	while (aux != NULL)
+	{
+		if (aux->indice==chave){
+			return aux;
+		}
+		aux = aux->next;
+	}
+	return NULL;
+}
+
+void inserirAllIndices(Lista* l){
+	for (int i = 0; i < CHAVES; i++){
+		verificarErro(inserirIndice(l,l->tail));
+	}
+}
+
+int hash(char nome[]){
+	int key=0;
+	for (int i = 0; i < strlen(nome); i++){
+		key += nome[i];
+	}
+	return (key*31)%53;
+}
+
+FILE* carregarArquivo(){
+FILE *file;
+file = fopen("nomes.txt", "r");
+}
+
+void inserirAllNomes(Lista* l){
+	int cont=0;
+	FILE* file = carregarArquivo();
+	char nome[20];
+	if (file!=NULL) {
+		while(fgets(nome, 100, file) != NULL){ 
+		if (nome[strlen(nome)-1]=='\n'){
+			nome[strlen(nome)-1] = '\0';
+		}
+			chamarInserir(l,nome);
+		}
+	}
+	ordenarAll(l);
+}
+
+void ordenarAll(Lista* l){
+	Indice* aux = l->head;
+
+	while (aux != NULL)
+	{
+		quickSort(aux,0,aux->size-1);
+		aux = aux->next;
+	}
+}
+
+Indice* chamarInserir(Lista* l,char nome[]){
+	Indice* i = encontrarIndice(l,hash(nome));
+			if (i!=NULL){
+				verificarErro(inserirDado(nome,i,i->tail));
+			}
+	return i;
+}
+
+void imprimirSizes(Lista* l){
+	Indice* aux = l->head;
+
+	while (aux != NULL)
+	{
+		printf("\n Chave: %i - Size: %i",aux->indice, aux->size);
+		aux = aux->next;
+	}
+}
+
+void imprimirPosicaoNome(Lista* l, char nome[]){
+	int chave = hash(nome);
+	int pos = encontrarPosicaoNome(encontrarIndice(l,chave),nome);
+	if (pos<0){
+		printf("\nO nome %s nao esta na lista.",nome);
+	} else{
+		printf("\nO nome %s esta na posicao %i na lista de chave %i.",nome,pos,chave);
+	}
+	
+}
+
+int encontrarPosicaoNome(Indice* i, char nome[]){
+	Elemento *aux;
+	aux = i->head;
+	int cont=0;
+	while (aux != NULL)
+	{
+	if (strcmp(nome, aux->nome)==0){
+		return cont;
+	} 
+		aux = aux->next;
+		cont++;
+	}
+	return -1;
+}
+
+Elemento* encontrarElemento(Indice* i, char nome[]){
+	Elemento *aux;
+	aux = i->head;
+
+	while (aux != NULL)
+	{
+		if (strcmp(aux->nome,nome)==0){
+			return aux;
+		}
+		aux = aux->next;
+	}
+	return NULL;
+}
+
+Indice* removerNomeEspecifico(Lista* l, char nome[]){
+	Indice* i = encontrarIndice(l,hash(nome));
+	Elemento *e = encontrarElemento(i,nome);
+	
+	if (e!= NULL){
+		remover(i,e);
+	} else{
+		printf("Nome nao existe");
+		return NULL;
+	}
+	return i;
+}
+
+Elemento* encontrarMeio(int c,Indice* in){
+	Elemento *aux;
+	aux = in->head;
+	int cont=0;
+	while (aux != NULL){
+	if (cont==c){
+		return aux;
+	} 
+		aux = aux->next;
+		cont++;
+	}
+}
+
+void quickSort(Indice *in, int inicio, int fim) {
+  	int  i = inicio;
+    int j = fim;
+    Elemento* x = encontrarMeio((inicio + fim) / 2,in) ;
+	Elemento* auxInicio = encontrarMeio(inicio,in);
+	Elemento* auxFinal = encontrarMeio(fim,in);
+     
+    while(i <= j) {
+        while((strcmp(auxInicio->nome,x->nome)< 0)) {
+            i++;
+			auxInicio = auxInicio->next;
+        }
+        while((strcmp(auxFinal->nome,x->nome)> 0)) {
+			auxFinal = auxFinal->prev;
+            j--;
+        }
+
+        if(i <= j) {
+			changeNome(auxInicio,auxFinal);
+            i++;
+            j--;
+        }
+    }
+     
+    if(j > inicio) {
+        quickSort(in, inicio, j);
+    }
+    if(i < fim) {
+        quickSort(in, i, fim);
+    }
+}
+
+void changeNome(Elemento* x, Elemento* y){
+	Elemento* e = alocarElemento();
+
+	strcpy(e->nome,y->nome);
+	strcpy(y->nome,x->nome);
+	strcpy(x->nome,e->nome);
+
+	y = y->prev;
+	x = x->next;
+	free(e);
+}
+
+char* lerNome(){
+	char *nome = malloc(sizeof(char) * 20);
+	printf("Digite o nome:");
+	scanf("%s",nome);
+	return nome;
+}
+
+void menu(Lista *l){
+	int op=-1;
+	char nome[20];
+
+	while (op!=0){
+		op = imprimirMenu();
+		switch (op){
+		case 1: // percorrer Indice
+			printf("\nDigite o indice que deseja acessar:\n");
+			int i;
+			scanf("%i",&i);
+			percorrerIndice(encontrarIndice(l,i));
+			break;
+
+		case 2: // inserir novo nome
+			strcpy(nome,lerNome());
+			Indice* in = chamarInserir(l,nome);
+			quickSort(in,0,in->size-1);
+			percorrerIndice(in);
+			break;
+
+		case 3:// qtd elementos por chave
+			imprimirSizes(l); 
+			break;
+
+		case 4: //remover nome
+			strcpy(nome,lerNome());
+			Indice* ind = removerNomeEspecifico(l,nome);
+			percorrerIndice(ind);
+			break;
+
+		case 5: //encontrar posicao nome
+			strcpy(nome,lerNome());
+			imprimirPosicaoNome(l,nome);
+			break;
+		
+		case 0: //sair
+			printf("\nFinalizando\n");
+			break;
+
+		default:
+			printf("Opcao informada invalida");
+			break;
+		}
+	}
+}
+
+int imprimirMenu(){
+	int op=-1;
+	printf("\nDigite a opcao escolhida:\n");
+	printf("\n1 - Percorrer Chave ");
+	printf("\n2 - Inserir Novo Nome");
+	printf("\n3 - Imprimir Quantidade de Elementos em Cada Chave");
+	printf("\n4 - Remover um Nome");
+	printf("\n5 - Encontrar a Posicao de um Nome");
+	printf("\n0 - Sair\n");
+	scanf("%i",&op);
+
+	return op;
+}
+
+void verificarErro(int erro){
+	if (erro==-1){
+		printf("Impossível alocar Memória");
+	} else if(erro==-2){
+		printf("Impossível Inserir");
+	}
+}
